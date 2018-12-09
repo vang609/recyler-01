f4feeb19a57a352772e57049fa97a1e1a044fcba
vang <enrique.vangz@gmail.com>
2018-12-09 16:08:49 -0500

    First commit Recycler

---
+++ src/app/pages/graphic01/graphic01.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphic01',
+  templateUrl: './graphic01.component.html',
+  styles: []
+})
+export class Graphic01Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
